c3666b5a5741d9ac25f5e6562e835281ff857f65
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-10-20 17:00:48 +0330

    window created.

--- super_market_app.py
+++ super_market_app.py
@@ -3,31 +3,21 @@ import tkinter
 from tkinter import *
 
 
-try:
-    name = input("Enter your name: ")
-    name = name_validator(name)
-    id= int(input("Enter your family: "))
-    id = id_validator(id_number)
-    price = float(input("Enter your age: "))
-    price_validator(price)
-except:
-    print("Invalid data !!!")
-
 window = Tk()
 window.title("Name id price Window")
 window.geometry("600x400")
 
-Label = Label(window, text="name: ").place(relx=20, rely=20)
+Label = Label(window, textvariable="name: ").place(x=20, y=20)
 name = StringVar()
-Entry = Entry(window, textvariable=name).place(relx=100, rely=40)
+Entry = Entry(window, textvariable=name).place(x=100, y=40)
 
-label = Label(window, text="ID: ").place(relx=20, rely=60)
+label = Label(window, textvariable="ID: ").place(x=20, y=60)
 entry = Entry(window).place(relx=100, rely=60)
-id_number =
+id_number = IntVar()
 
-label = Label(window, text="price: ").place(relx=20, rely=100)
-price =
-entry = Entry(window).place(relx=100, rely=100)
+label = Label(window, textvariable="price: ").place(x=20, y=100)
+price = IntVar()
+entry = Entry(window).place(x=100, y=100)
 
-button = Button(window).place(relx=100, rely=200)
+button = Button(window).place(x=100, y=200)
 window.mainloop()
