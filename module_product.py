2ed5562407840fc224ca49d94582dbdb5afcc55e
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-10-24 02:13:50 +0330

    window updated.

--- module_product.py
+++ module_product.py
@@ -1,18 +1,3 @@
-#import re
-
-
-#def name_validator(name):
- #   if not re.match(r"^[a-zA-Z\s]{3,30}$", name):
-  #      raise ValueError("Invalid Name !!!")
-
-#def id_validator(id_number):
- #   if not re.match(r"^[\d\s]{3,30}$", id_number):
-  #      raise ValueError("Invalid Id Number !!!")
-
-#def price_validator(price):
- #   if not (type(price) == int and 0 < price < 150):
-  #      raise ValueError("Invalid price !!!")
-
 import re
 from main import name, brand, id_number, price, quantity
 product_list = []
@@ -24,8 +9,8 @@ def brand_validator():
     if not re.match(r"^[a-zA-Z\s]{2,20}$" , brand):
         raise ValueError("Brand must be a string")
 def id_validator(id_number):
-    if not re.match(r"^[\d\s]{3,30}$", id_number):
-        raise ValueError("Invalid Id Number !!!")
+    if not re.match(r"^[\d\s]{10}$", id_number):
+        raise ValueError("Id Number must be a ten digit number !!!")
 def price_validator():
     if not 0<price:
         raise ValueError("Price must be a positive number")
