8b755ca97c2b1abf42a12fdefdf55767e9aedfa7
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-10-25 16:19:08 +0330

    Super market window updated.

--- module_product.py
+++ module_product.py
@@ -6,19 +6,29 @@ quantity = 0
 product_list = []
 
 def name_validator(name):
-    if not re.match(r"^[a-zA-Z\s]{2,20}$",name):
+    if re.match(r"^[a-zA-Z\s]{2,20}$",name):
+        return name
+    else:
         raise ValueError("Name must be a string")
 def brand_validator(brand):
-    if not re.match(r"^[a-zA-Z\s]{2,20}$" , brand):
+    if re.match(r"^[a-zA-Z\s]{2,20}$" , brand):
+        return brand
+    else:
         raise ValueError("Brand must be a string")
 def id_validator(id_number):
-    if not re.match(r"^[\d\s]{3,30}$", id_number):
+    if re.match(r"^[\d\s]{3,30}$", id_number):
+        return id_number
+    else:
         raise ValueError("Invalid Id Number !!!")
 def price_validator(price):
-    if not (type(price) == int):
+    if (type(price) == int):
+        return price
+    else:
         raise ValueError("Price must be a positive number")
 def quantity_validator(quantity):
-    if not (type(quantity) == int):
+    if (type(quantity) == int):
+        return quantity
+    else:
         raise ValueError("Quantity must be a positive number")
 def expire_validator(expire_date):
     if not re.match(r"[0-9]{4}/[0-9]{2}/[0-9]{2}", expire_date):
