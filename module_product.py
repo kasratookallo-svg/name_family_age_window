5dfff7575079ae88a6c5d774a603c91ede4a37c5
Messbah.A <Messbah.A@Gmail.com>
2025-10-25 16:44:01 +0330

    messbah debugged

--- module_product.py
+++ module_product.py
@@ -1,36 +1,44 @@
 import re
-import datetime
+from datetime import date
 
-price = 0
-quantity = 0
 product_list = []
 
+
 def name_validator(name):
-    if re.match(r"^[a-zA-Z\s]{2,20}$",name):
+    if re.match(r"^[a-zA-Z\s]{2,20}$", name):
         return name
     else:
         raise ValueError("Name must be a string")
+
+
 def brand_validator(brand):
-    if re.match(r"^[a-zA-Z\s]{2,20}$" , brand):
+    if re.match(r"^[a-zA-Z\s]{2,20}$", brand):
         return brand
     else:
         raise ValueError("Brand must be a string")
+
+
 def id_validator(id_number):
-    if re.match(r"^[\d\s]{3,30}$", id_number):
+    if type(id_number) == int and id_number > 0:
         return id_number
     else:
         raise ValueError("Invalid Id Number !!!")
+
+
 def price_validator(price):
-    if (type(price) == int):
+    if type(price) == float and price > 0:
         return price
     else:
         raise ValueError("Price must be a positive number")
+
+
 def quantity_validator(quantity):
-    if (type(quantity) == int):
+    if type(quantity) == int and quantity > 0:
         return quantity
     else:
         raise ValueError("Quantity must be a positive number")
-def expire_validator(expire_date):
-    if not re.match(r"[0-9]{4}/[0-9]{2}/[0-9]{2}", expire_date):
-        raise ValueError("Expiration date must be YYYY/MM/DD.")
 
+
+def expire_validator(expire_date):
+    if type(expire_date) != date and expire_date >= date.today():
+        raise ValueError("Expiration date must be YYYY/MM/DD.")
