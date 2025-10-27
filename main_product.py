2d3a332fdf0b752de37c21c599a1ded639f19030
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-10-27 14:32:11 +0330

    Super market window updated.

--- main_product.py
+++ main_product.py
@@ -1,7 +1,13 @@
 from datetime import date, datetime
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
+
+
+#todo from persiantools.digits import to_word
+
 from module_product import *
+from tkinter import ttk
 
 product_list = []
 
@@ -26,8 +32,9 @@ def save():
             "expire_date": expire_date}
 
         product_list.append(product)
+        table.insert("", END, values = tuple(product.values()))
         messagebox.showinfo("Information Saved", "Product saved successfully.")
-        id_number.set(0)
+        id_number.set(len(product_list)+1)
         name.set("")
         brand.set("")
         quantity.set(0)
@@ -46,19 +53,19 @@ def total_price():
     total = 0
     for product in product_list:
         total = total + product['quantity'] * product['price']
-    messagebox.showinfo("Total Price", f"Total value of all products: {total}")
+    messagebox.showinfo("Total Price", f"Total value of all products: {total}")  #todo{to_word(total)}
 
 
 
 
 window = Tk()
-window.geometry("350x350")
+window.geometry("850x350")
 window.title("Product Management")
 
 # Identity Number
 Label(window, text="ID num.").place(x=40, y=20)
 id_number = IntVar()
-Entry(window, textvariable=id_number).place(x=180, y=20)
+Entry(window, textvariable=id_number, state="readonly").place(x=180, y=20)
 
 # Name
 Label(window, text="Name").place(x=40, y=60)
@@ -88,5 +95,23 @@ Entry(window, textvariable=expiration_date).place(x=180, y=220)
 # Buttons
 Button(window, text="Submit", command=save).place(x=40, y=300, width=100)
 Button(window, text="Total", command=total_price).place(x=200, y=300, width=100)
+
+table = ttk.Treeview(window , columns=(1,2,3,4,5,6),height = 14,  show="headings")
+table.heading(1, text="ID")
+table.heading(2, text="Name")
+table.heading(3, text="Brand")
+table.heading(4, text="Quantity")
+table.heading(5, text="Price")
+table.heading(6, text="Expiration date")
+
+table.column(1, width=60)
+table.column(2, width=100)
+table.column(3, width=100)
+table.column(4, width=30)
+table.column(5, width=30)
+table.column(6, width=100)
+
+
+table.place(x=400, y=20)
 
 window.mainloop()
